70ee2cc4dc3e19d90d6dbf21c197ba4094bb6343
Alex Jacobs <alexjquest@gmail.com>
2024-07-16 13:23:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"element={<Home/>}/>
+        <Route path="/simply-glam"element={<Home/>}/>
       </Routes>
     </BrowserRouter>
 
