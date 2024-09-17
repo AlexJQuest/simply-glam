d0ec45a678253ddd7e685b56359db92856f8a25c
Alex Jacobs <alexjquest@gmail.com>
2024-09-17 23:14:03 +0200

    Update styles again almost done

--- src/components/Coursell/coursell.jsx
+++ src/components/Coursell/coursell.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./coursell.css"
+import './coursell.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
 import image1 from './WhatsApp Image 2024-07-12 at 11.14.19 AM.jpeg';
@@ -9,19 +9,26 @@ import image4 from './WhatsApp Image 2024-07-12 at 11.14.23 AM.jpeg';
 import image5 from './WhatsApp Image 2024-07-12 at 11.14.24 AM.jpeg';
 import image6 from './WhatsApp Image 2024-07-12 at 11.14.21 AM (1).jpeg';
 import image7 from './WhatsApp Image 2024-07-12 at 11.14.22 AM.jpeg';
+
 const CarouselComponent = () => {
-    const images = [image1, image2, image3, image4, image5, image6 , image7]; // Store image paths in an array
+    const images = [image1, image2, image3, image4, image5, image6, image7]; // Store image paths in an array
 
     return (
-      <div className="carousel-container">
-        <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img src={image} alt={`Slide ${index + 1}`} />
-            </div>
-          ))}
-        </Carousel>
-      </div>
+        <div className="carousel-container">
+            <Carousel 
+                showArrows={true} 
+                autoPlay={true} 
+                infiniteLoop={true} 
+                showThumbs={false}
+                swipeable={true}
+            >
+                {images.map((image, index) => (
+                    <div key={index} className="carousel-item">
+                        <img src={image} alt={`Slide ${index + 1}`} className="carousel-image" />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
     );
 };
 
